a04c7ed70f160f7f5c7a04dbb898faa94e4e4296
vincentcheri <vj9783@gmail.com>
2022-09-13 14:21:07 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*_putchar - prints characters as they are.
+*@n: Character to be printed
+*Return: 0 if exited properly, non-zero otherwise
+*/
+int _putchar(char n)
+{
+    printf("%c", n);
+    return (0);
+}
