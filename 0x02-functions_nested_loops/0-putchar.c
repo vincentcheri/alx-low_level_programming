45a177ea59de3548b36728ae8a2a040752632681
vincentcheri <vj9783@gmail.com>
2022-09-13 13:45:43 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*main - Prints character
+*
+*Return: 0 if exited properly, non-zero otherwise
+*/
+int main(void)
+{
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return (0);
+}
