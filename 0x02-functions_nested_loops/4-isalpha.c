88b067593c511f7db1929047865a678b28674d4e
vincentcheri <vj9783@gmail.com>
2022-09-14 08:42:01 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * @c: the character to be checked
  * Return: 1 if c is a letter, non-zero otherwise
 */
-nt _isalpha(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') ||
 	    (c >= 'A' && c <= 'Z'))
