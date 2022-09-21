72b9a8dadf58f601c460393ac98a63692025c0ae
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2022-09-21 22:25:34 +0100

    cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* cap_string - Capitalize all words of a string
+* @s: the string to read
+* Return: s
+*/
+
+char *cap_string(char *s)
+{
+int c = 0;
+
+while (s[c])
+{
+while (!(s[c] >= 'a' && s[c] <= 'z'))
+c++;
+
+if (s[c - 1] == ',' ||
+s[c - 1] == ' ' ||
+s[c - 1] == '\t' ||
+s[c - 1] == '\n' ||
+s[c - 1] == ';' ||
+s[c - 1] == '.' ||
+s[c - 1] == '!' ||
+s[c - 1] == '?' ||
+s[c - 1] == '"' ||
+s[c - 1] == '(' ||
+s[c - 1] == ')' ||
+s[c - 1] == '{' ||
+s[c - 1] == '}' ||
+c == 0)
+s[c] = s[c] - 32;
+c++;
+}
+return (s);
+}
