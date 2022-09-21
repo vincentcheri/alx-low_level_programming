e2ab80ebcc6032a481f908abf2809158d8d6feaf
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2022-09-21 23:10:03 +0100

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
 * rot13 - encodes a string using rot 13
@@ -7,23 +7,24 @@
 */
 char *rot13(char *s)
 {
-int i = 0;
+	int i = 0;
 
-while (s[i] != '\0')
-{
-if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
-{
-s[i] += 13;
-}
+	while (s[i] != '\0')
+	{
+		if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+		{
+			s[i] += 13;
+		}
 
-else
-{
-while ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
-{
-s[i] -= 13;
+		else
+		{
+			while ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
+			{
+				s[i] -= 13;
+			}
+		}
+		i++;
+	}
+	return (s);
 }
-}
-i++;
-}
-return (s);
-}
+Footer
