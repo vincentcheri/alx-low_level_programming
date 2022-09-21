acf56e78e930c98479a2cb895c208df4592d9b44
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2022-09-21 22:29:54 +0100

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* rot13 - encodes a string using rot 13
+* @s: The string to encode
+* Return: (s) string
+*/
+char *rot13(char *s)
+{
+int i = 0;
+
+while (s[i] != '\0')
+{
+if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+{
+s[i] += 13;
+}
+
+else
+{
+while ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
+{
+s[i] -= 13;
+}
+}
+i++;
+}
+return (s);
+}
+Footer
