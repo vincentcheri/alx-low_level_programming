a492ac98b2e200e9fdf4680dfad17dfd3ab6e3ab
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2022-09-21 23:30:21 +0100

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,23 +7,23 @@
 */
 char *rot13(char *s)
 {
-	int i = 0;
+int i = 0;
 
-	while (s[i] != '\0')
-	{
-		if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
-		{
-			s[i] += 13;
-		}
+while (s[i] != '\0')
+{
+if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+{
+s[i] += 13;
+}
 
-		else
-		{
-			while ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
-			{
-				s[i] -= 13;
-			}
-		}
-		i++;
-	}
-	return (s);
+else
+{
+while ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
+{
+s[i] -= 13;
+}
+}
+i++;
+}
+return (s);
 }
