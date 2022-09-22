6a7763f57df2c06c2a2afcf4c4bd995fc0a245d4
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2022-09-22 22:54:23 +0100

    comments added

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,12 +14,12 @@ for (j = 0; j < 100; j++)  /*second list of numbers from 1 t0 99*/
 {
 if (i < j)         /*we only want combinations where j is greater than i.*/
 {
-putchar((i / 10) + 48); /*divides each number in the list of the first loop starting from "0" and adds 48(ascii value for "0")*/
-putchar((i % 10) + 48); /*modulus of each number in the list of the first loop starting from "0" and adds 48(ascii value for "0")*/
+putchar((i / 10) + 48); /*divides each number in the list of the first loop starting from "0" and adds 48(ascii value for "0") to the result*/
+putchar((i % 10) + 48); /*modulus of each number in the list of the first loop starting from "0" and adds 48(ascii value for "0") to the result*/
 putchar(' ');
-putchar((j / 10) + 48); /*divides each number in the list of the second loop starting from "0" and adds 48(ascii value for "0")*/
-putchar((j % 10) + 48); /*modulus of each number in the list of the second loop starting from "0" and adds 48(ascii value for "0")*/
-if (i != 98 || j != 99) /*this line avoids comma from been printed after the last combination. you can use i<98 || j<99*/
+putchar((j / 10) + 48); /*divides each number in the list of the second loop starting from "0" and adds 48(ascii value for "0") to the result*/
+putchar((j % 10) + 48); /*modulus of each number in the list of the second loop starting from "0" and adds 48(ascii value for "0") to the result*/
+if (i != 98 || j != 99) /*this line avoids comma from been printed after the last combination. you can use i<98 || j<99. ab cd, ef gh and ! ab cd, ef gh,*/
 {
 putchar(',');
 putchar(' ');
