5b9cfd89b87bde87bffbce3792531573e7804131
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2022-09-22 18:29:59 +0100

    comments added

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,24 +6,24 @@
  */
 int main(void)
 {
-	int n, m;
+int n, m;
 
-	for (n = 48; n <= 56; n++)
-	{
-		for (m = 49; m <= 57; m++)
-		{
-			if (m > n)
-			{
-				putchar(n);
-				putchar(m);
-				if (n != 56 || m != 57)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+for (n = 48; n <= 56; n++) /* gets number from 0 to 8. 48 to 56 is the ascii number for 1 to 8*/
+{
+for (m = 49; m <= 57; m++) /* gets number from 1 to 9. 49 to 57 is the ascii number for 1 to 9.*/
+{
+if (m > n)         /* where the combinations are the same do not print it out. only print where m is greater that n. so no 11 22 33 etc*/
+{
+putchar(n);    /* the first loop check if n is trully less than 8 starting from 0 and combine 0 with 1-9 of the second loop and keep updating to 1...8.*/				                *
+putchar(m);
+if (n != 56 || m != 57)
+{
+putchar(',');  /* for each combination printed put a "," */
+putchar(' '); /* after a combination and "," are printed print space */
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
