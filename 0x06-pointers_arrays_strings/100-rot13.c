f8bf78b0cc633247a3b8dc8d014fe77396b4aded
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2022-09-22 11:31:27 +0100

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,29 +1,46 @@
-include "main.h"
+#include "holberton.h"
+
 /**
- * rot13 - encodes a string using rot13
+ * rot13 - Encodes a string using rot13.
+ * @str: The string to be encoded.
  *
- * @z: the string to encode
- *
- * Return: the new string
-*/
+ * Return: A pointer to the encoded string.
+ */
+char *rot13(char *str)
+{
+	int indx1 = 0, indx2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
 
-char *rot13(char *z)
-{
-int i, j;
-char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-char n[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	while (str[indx1])
+	{
+		for (indx2 = 0; indx2 < 52; indx2++)
+		{
+			if (str[indx1] == alphabet[indx2])
+			{
+				str[indx1] = rot13key[indx2];
+				break;
+			}
+		}
 
-for (i = 0; z[i] != '\0'; i++)
-{
-for (j = 0; j < 56; j++)
-{
-if (z[i] == a[j])
-{
-z[i] = n[j];
-break;
-}
-}
-}
+		indx1++;
+	}
 
-return (z);
+	return (str);
 }
