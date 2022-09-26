17abbdfc7093c78a061ceb77764edf54ac352b8c
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2022-09-26 15:24:07 +0100

    update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -20,19 +20,5 @@ printf("Infinite loop incoming :(\n");
 * }
 */
 printf("Infinite loop avoided! \\o/\n");
-
-	int i;
-
-	printf("Infinite loop incoming :(\n");
-	/**
-	* i = 0;
-	*
-	* while (i < 10)
-	* {
-	* putchar(i);
-	* }
-	*/
-	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
