5441c4553e8461262f7b11734bbdf60658d373a1
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2022-09-29 17:46:45 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - checks numbers that are divisible by 5 and 3 separately. and numbers that both 3 and 5 can go in
- * Return: 0
-*/
+ * main - prints the numbers from 1 to 100, followed by a new line
+ * but for multiples of three prints Fizz instead of the number
+ * and for the multiples of five prints Buzz
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int a = 1;
