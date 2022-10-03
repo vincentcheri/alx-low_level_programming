c2c502437e099235a03b2cdc8ee79adcbcbbca31
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2022-10-03 15:11:41 +0100

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,11 +7,11 @@
 *Return: always (0)
 */
 
-int main(int argc__attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 
 int i;
-for(i=0; argv[i] != '\0'; i++)
+for (i=0; argv[i] != '\0'; i++)
 {
 
 printf("%s\n", argv[i]);
