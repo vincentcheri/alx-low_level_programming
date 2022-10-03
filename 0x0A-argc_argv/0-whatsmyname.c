771656814b9c63a22f2b2aff122614ec5a3f3ef6
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2022-10-03 13:47:13 +0100

    my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
 * main - Prints the name of the programme file
 * @argc: This is the argument counter
 * @argv: This is the argument vector
-* Return: Always 0 (Success)
+* Return: Always 0 (Success).
 */
 
 int main(int argc __attribute__((unused)), char *argv[])
