c606a2edc3c2f45c5a6f1c5fb414ddabb0df478a
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2022-10-14 17:34:36 +0100

    added some comment

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -15,15 +15,15 @@ int len, i;
 
 len = 0;
 
-while (src[len] != '\0')
+while (src[len] != '\0')         /*this while loop gets the lenght of the src*/
 {
 len++;
-}
+}                                 /*this while loop stops here*/
 
-	for (i = 0; i < len; i++)
+	for (i = 0; i < len; i++)  /*this for loop conditioned the length of src from 0 to 'len' which is the index of the last char before the terminating null byte.*/
 	{
-	dest[i] = src[i];
-}
+	dest[i] = src[i];         /*this line asigns elements from src for each of the index of dest[98] stopping where i < len*/
+}                                 /*loop ends/
 dest[i] = '\0';
 
 return (dest);
