11f2aeda8eafc8c12ff9a37f3a22b24ba756f494
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2022-11-02 19:00:14 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,11 +14,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 
 	if (filename == NULL)
+	{
 		return (0);
+	}
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-		return (0);
+	{
+	        return (0);
+	}
 
 	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
