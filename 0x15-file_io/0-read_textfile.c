0b2657c87fa8363e349ed220302f11d35207a67d
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2022-11-03 08:02:35 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,39 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * read_textfile - Entry Point
  * @filename: file name
  * @letters: size
  * Return: 0
- */
+*/
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file, rd, wr;
-	char *buf;
+	ssize_t file, let, w;
+	char *text;
 
-	if (filename == NULL)
+	text = malloc(letters);
+	if (text == NULL)
+	{
 		return (0);
-
+	}
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	
 	file = open(filename, O_RDONLY);
 
 	if (file == -1)
+	{
+		free(text);
 		return (0);
+	}
 
-	buf = malloc(sizeof(char) * letters + 1);
-	if (buf == NULL)
-		return (0);
-
-	rd = read(file, buf, letters);
-	if (rd == -1)
-		return (0);
-
-	buf[letters] = '\0';
-
-	wr = write(1, buf, rd);
-	if (wr == -1)
-		return (0);
-
+	let = read(file, text, letters);
+	w = write(STDOUT_FILENO, text, let);
 	close(file);
-	free(buf);
-	return (wr);
+	return (w);
 }
