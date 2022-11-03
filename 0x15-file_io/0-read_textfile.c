572491f6ff48de21d29d11e8242f8e67fbeccc5b
vincentcheri <vj9783@gmail.com>
2022-11-03 00:19:53 -0700

    trailing whitespace removed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,43 +6,34 @@
  * read_textfile - Reads a text file and prints it to POSIX stdout.
  * @filename: A pointer to the name of the file.
  * @letters: The number of letters the function should read and print.
- *
- * Return: If the function fails or filename is NULL - 0. O/w - the actual number of bytes the function can read and print.
-*/
-/**
- * read_textfile - Entry Point
- * @filename: file name
- * @letters: size
- * Return: 0
+ * Return: If the function fails or filename is NULL - 0.
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t file, let, w;
-	char *text;
+ssize_t file, let, w;
+char *text;
+text = malloc(letters);
+if (text == NULL)
+{
+return (0);
+}
+if (filename == NULL)
+{
+return (0);
+}
+file = open(filename, O_RDONLY);
 
-	text = malloc(letters);
-	if (text == NULL)
-	{
-		return (0);
-	}
-	if (filename == NULL)
-	{
-		return (0);
-	}
-	
-	file = open(filename, O_RDONLY);
+if (file == -1)
+{
+free(text);
+return (0);
+}
 
-	if (file == -1)
-	{
-		free(text);
-		return (0);
-	}
+free(buffer);
+close(o);
 
-	free(buffer);
-	close(o);
-
-	let = read(file, text, letters);
-	w = write(STDOUT_FILENO, text, let);
-	close(file);
-	return (w);
+let = read(file, text, letters);
+w = write(STDOUT_FILENO, text, let);
+close(file);
+return (w);
 }
