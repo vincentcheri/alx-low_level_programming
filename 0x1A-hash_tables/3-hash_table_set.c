8ef41aa47ddfd066c6840f10e223b551750adf50
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-03-18 15:52:24 +0100

    fix documentation style

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,11 +1,11 @@
 /*
  * File: 3-hash_table_set.c
  * Auth: Brennan D Baraban
-*/
+**/
 
 #include "hash_tables.h"
 
-/**
+/*
  * hash_table_set - Add or update an element in a hash table.
  * @ht: A pointer to the hash table.
  * @key: The key to add - cannot be an empty string.
@@ -13,7 +13,7 @@
  *
  * Return: Upon failure - 0.
  *         Otherwise - 1.
- */
+ **/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
