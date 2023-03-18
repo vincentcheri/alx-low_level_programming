77cd17af99cbff7fe01a5dc2625bf65fe4aff0e6
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-03-18 15:53:39 +0100

    fix documentation

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,5 @@
 /*
  * File: 3-hash_table_set.c
- * Auth: Brennan D Baraban
 **/
 
 #include "hash_tables.h"
@@ -10,10 +9,9 @@
  * @ht: A pointer to the hash table.
  * @key: The key to add - cannot be an empty string.
  * @value: The value associated with key.
- *
- * Return: Upon failure - 0.
- *         Otherwise - 1.
+ * Return: Upon failure - 0. Otherwise - 1.
  **/
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
