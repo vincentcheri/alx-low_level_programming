fe8ea076b16be51677bbe4c4bcf9a78a24e5a311
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-03-18 13:24:23 +0100

    djb2

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,15 +1,9 @@
-/*
- * File: 1-djb2.c
- * 
- */
-
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - Hash function implementing the djb2 algorithm.
- * @str: The string to hash.
- *
- * Return: The calculated hash.
+ * hash_djb2 - function to implement djb2 algorithm
+ * @str: string to be used as hash
+ * Return: The new hash
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
@@ -18,7 +12,8 @@ unsigned long int hash_djb2(const unsigned char *str)
 
 	hash = 5381;
 	while ((c = *str++))
+	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-
+	}
 	return (hash);
 }
