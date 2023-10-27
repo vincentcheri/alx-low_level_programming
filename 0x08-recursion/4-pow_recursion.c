525a88803297a4e1fa9f998444efc6bbed850089
vincentcheri <vj9783@gmail.com>
2023-10-27 06:54:49 -0700

    raising a base to an exponent

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: the number
+ * @y: the power
+ * Return: value of x raised to the power of y and -1 if y < 0 & 1 if y == 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	return (_pow_recursion(x, y - 1))
+}
