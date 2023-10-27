a2e2f6d1004add0a0b288a2a1f9581b7626cbabc
vincentcheri <vj9783@gmail.com>
2023-10-27 06:26:24 -0700

    added description for @n

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,6 +3,7 @@
 /**
  * factorial - finds the factorial of a number n
  * Return:  factorial of a number, -1 if n < 0, 1 if n == 0
+ * @n: the parameter which will reveive argument for factorial computation
  */
 
 int factorial(int n)
