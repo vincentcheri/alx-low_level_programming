af9e83d0ae86c51b225e565ea18a38b012ead843
vincentcheri <vj9783@gmail.com>
2023-11-05 10:54:12 -0800

    mem check

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of block of memory to be allocated
+ * @Return: exit code 98 and a pointer to the memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == 0)
+		exit(98);
+
+	return (ptr);
+}
+
