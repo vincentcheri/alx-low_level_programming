44deed3f2d2e7c535e30471d820b396d9a1e729f
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-11-06 20:28:31 +0100

    initializing members of a struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,7 +7,7 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: name of the owner of the dog
- * Return: nothing
+ * Return: 0
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
