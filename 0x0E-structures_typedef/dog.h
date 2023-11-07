5b11ed449f4e6d59eb0294d5c972ea58d17359ad
vincentcheri <vj9783@gmail.com>
2023-11-07 03:26:17 -0800

    struct dog --> dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,12 @@
  * Description: this struct encapsulate's the name,age and owner of a dog.
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
