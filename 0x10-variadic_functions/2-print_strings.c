b02a57f32c00a2724c214a7881c7f6d530720a65
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-11-10 13:31:16 +0100

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,11 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		}
 
-		else
-		{
-			printf("%s", string_atm);
-
-		}
+		printf("%s", string_atm);
 
 		if (i < n - 1 && separator != NULL)
 		{
