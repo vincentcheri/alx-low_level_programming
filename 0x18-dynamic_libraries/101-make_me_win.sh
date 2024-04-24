d7d3874df5ac182423ae457bcb4ea6f960c6e8ae
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2024-04-24 10:01:59 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/libtest.so https://github.com/MitaliSengupta/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libtest.so
+wget -q -O /tmp/libtest.so https://github.com/vincentcheri/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libtest.so
 export LD_PRELOAD=/tmp/libtest.so
