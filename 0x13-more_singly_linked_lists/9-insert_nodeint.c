1bbac596f2f022ca57634737a54fa3d5c7e9267c
vincentcheri <vj9783@gmail.com>
2024-06-25 16:57:54 +0100

    removed unindented tabs

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,45 +10,44 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *new_node;
+	listint_t *current;
+	unsigned int i;
 
-    listint_t *new_node, *current;
-    unsigned int i;
+	new_node = malloc(sizeof(listint_t));
 
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-        return NULL;
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 
-    new_node->n = n;
-    new_node->next = NULL;
+	new_node->n = n;
+	new_node->next = NULL;
 
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
-    if (idx == 0)
-    {
-        new_node->next = *head;
-        *head = new_node;
-        return new_node;
-    }
+	current = *head;
+	for (i = 0; i < idx - 1; i++)
+	{
+		if (current == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		current = current->next;
+	}
 
-    current = *head;
+	if (current == NULL)
+	{
+		return (NULL);
+	}
 
-    for (i = 0; i < idx - 1; i++)
-    {
-        if (current == NULL)
-        {
-            free(new_node);
-            return NULL;
-        }
-        current = current->next;
-    }
-
-
-    if (current == NULL)
-    {
-        return NULL;
-    }
-
-    new_node->next = current->next;
-    current->next = new_node;
-
-    return new_node;
+	new_node->next = current->next;
+	current->next = new_node;
+	return (new_node);
 }
